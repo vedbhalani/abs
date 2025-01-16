62905ae8faac1570c270a685e5d64fbf2477f41c
vedbhalani <vedbhalani21@gmail.com>
2025-01-16 15:12:35 +0530

    Add files via upload

---
+++ 131a.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int c=0;
+    string w=s;
+    for(int i=0;i<s.length();i++){
+        if(s[i]>='A'&&s[i]<='Z') c++;
+    }
+    if(c==s.length()){
+        for(int i=0;i<s.length();i++){
+       w[i]=tolower(s[i]);
+    }
+    }
+    else if(c==s.length()-1){
+        if(s[0]>='a'&&s[0]<='z'){
+        w[0]=s[0]-32;
+        for(int i=1;i<s.length();i++){
+       w[i]=tolower(s[i]);
+    }
+
+    }
+    }
+
+    cout<<w<<endl;
+    return 0;
+}
